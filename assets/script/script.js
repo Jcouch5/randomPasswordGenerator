48c8dd459737adff2ee3fa57d4e6310a7f8f4076
jcouch <jasoncouchjr410@gmail.com>
2022-03-25 08:46:41 -0500

    removed characters from specialChars array

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,43 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // created array variables for each combination of Character types
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialsChar = ['!','@','#','$','%','^','&','*','(',')','_','+','-','=','[',,']','{','}',';',':','|','<','>','?'];
+var specialChars = ['!', '@','#','$','%','^','&','*','(',')','_','+','-','=']
 var lowerUpper = lowerCase.concat(upperCase);
 var lowerUpperNum = lowerUpper.concat(number);
-var all = lowerUpperNum.concat(specialsChar);
+var all = lowerUpperNum.concat(specialChars);
 var lowerNum = lowerCase.concat(number);
-var lowerSpecial = lowerCase.concat(specialsChar);
-var lowerUpperSpecial = lowerUpper.concat(specialsChar);
+var lowerSpecial = lowerCase.concat(specialChars);
+var lowerUpperSpecial = lowerUpper.concat(specialChars);
 var upperNum = upperCase.concat(number);
-var upperSpecial = upperCase.concat(specialsChar);
-var upperNumSpecial = upperNum.concat(specialsChar);
-var numberSpecial = number.concat(specialsChar);
+var upperSpecial = upperCase.concat(specialChars);
+var upperNumSpecial = upperNum.concat(specialChars);
+var numberSpecial = number.concat(specialChars);
 
 // Function that generators a password based on users responses
 function generatePassword() {
+
   // prompts user for number of characters in the password
   chars = prompt('How many Characters would you like? 8-128', 'Number between 8 and 128');
+
   // if user inputs a number higher than 128 an alert will come and they will be prompted again
   if(chars > 128) {
     alert('Please enter a number lower than 128');
     chars = prompt('How many Characters would you like? 8-128', 'Number between 8 and 128');
   } 
+
     // if user inputs a number lower than 8 an alert will come and they will be prompted again
   else if (chars < 8) {
     alert('Please enter a number higher than 8');
     chars = prompt('How many Characters would you like? 8-128', 'Number between 8 and 128');
   }
+
   // prompts user for whether or not they want lower case letters in the password
   lower = confirm('Would you like Lower case letters?');
+
   // // prompts user for whether or not they want upper case letters in the password
   upper = confirm('Would you like Upper case letters?', "Upper Case A-Z");
+
   // prompts user for whether or not they want numbers in the password
   num = confirm('Would you like Numbers?', "Number 0-9");
+
   // prompts user for whether or not they want special characters in the password
   specialChar = confirm('Would you like Special Characters?');
+
   // checks to make sure user selected at least character type
   // notifies the user if they made a mistake then prompts again
   if(!lower && !upper && !num && !specialChar) {
@@ -48,8 +57,9 @@ function generatePassword() {
     specialChar = confirm('Would you like Special Characters?');
   }
   var password = "";
+
   // iterates over the different combination available for the password to create a password
-  for (i=0; i <= chars; i++) {
+  for (i=0; i < chars; i++) {
     if(lower && upper && num && specialChar){
       randomNum = Math.floor(Math.random() * all.length);
       password += all[randomNum];
@@ -90,10 +100,11 @@ function generatePassword() {
       randomNum = Math.floor(Math.random() * number.length);
       password += number[randomNum];
     } else if (!lower && !upper && !num && specialChar) {
-      randomNum = Math.floor(Math.random() * specialsChar.length);
-      password += specialsChar[randomNum];
+      randomNum = Math.floor(Math.random() * specialChars.length);
+      password += specialChars[randomNum];
     }
   }
+
   // returns the password that was created
   return password;
 }
